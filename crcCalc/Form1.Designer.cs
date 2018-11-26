dd9e764a0588cd27cf5bfac55ba4b8e186dfb975
sai <sailaopoeng@azdigital.net>
2018-11-26 13:40:17 +0800

    First commit

---
+++ crcCalc/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace crcCalc
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtResultHex = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResultAscii = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(580, 193);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            this.txtInput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyUp);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(626, 248);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(500, 279);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtResultHex
+            // 
+            this.txtResultHex.Location = new System.Drawing.Point(12, 248);
+            this.txtResultHex.Name = "txtResultHex";
+            this.txtResultHex.ReadOnly = true;
+            this.txtResultHex.Size = new System.Drawing.Size(100, 20);
+            this.txtResultHex.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "HEX Result";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(623, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ascii Result";
+            // 
+            // txtResultAscii
+            // 
+            this.txtResultAscii.Location = new System.Drawing.Point(626, 12);
+            this.txtResultAscii.Multiline = true;
+            this.txtResultAscii.Name = "txtResultAscii";
+            this.txtResultAscii.ReadOnly = true;
+            this.txtResultAscii.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultAscii.Size = new System.Drawing.Size(580, 193);
+            this.txtResultAscii.TabIndex = 7;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 314);
+            this.Controls.Add(this.txtResultAscii);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultHex);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmMain";
+            this.Text = "crcCalc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtResultHex;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultAscii;
+    }
+}
+
---
+++ crcCalc/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crcCalc
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            doConvert();
+        }
+
+        private void doConvert()
+        {
+            if (txtInput.Text.Length > 0)
+            {
+                // char resultChar = new char();
+                string resultStr = "";
+                string resultHex = "";
+                string formatInput = "";
+                string formatInputStr = "";
+                //if (chkIsHex.Checked)
+                //{
+                //hex 
+                try
+                {
+                    byte[] data = FromHex(txtInput.Text);
+                    byte crc = crcFromBytes(data);
+                    byte[] crcAarray = new byte[1];
+                    crcAarray[0] = crc;
+                    resultStr = System.Text.Encoding.UTF8.GetString(crcAarray);
+                    resultHex = byteArrayToHexString(crcAarray);
+                    formatInput = byteArrayToHexString(data);
+
+                    // formatInputStr = System.Text.Encoding.UTF8.GetString(data); //System.Text.Encoding.UTF8.GetString(data);
+                     String tmp = Encoding.UTF8.GetString(data);
+                    char[] MyChars = tmp.ToCharArray();
+                    string sx = new string(MyChars);
+                    System.Diagnostics.Debug.WriteLine(sx);
+                    // int x = 0;
+                    // while (true)
+                    // {
+                        // for (int i = 0; i < MyChars.Length - x; i++)
+                        //     System.Diagnostics.Debug.WriteLine("{0}", MyChars[i]);
+                        // x++;
+                        // if (x == MyChars.Length)
+                            // break;
+                        
+                    // }
+                    
+                    var sb = new StringBuilder("new byte[" + data.Length + "] { ");
+                    int index = 0;
+                    foreach (var b in data)
+                    {
+                        try
+                        {
+                            byte[] tmpArray = new byte[1];
+                            tmpArray[0] = b;
+                            string rr = System.Text.Encoding.UTF8.GetString(tmpArray, 0, 1);
+                            char[] MyChars2 = rr.ToCharArray();
+                            string s = new string(MyChars2);
+                            sb.AppendFormat("{0}-{1},", index, s);
+                            System.Diagnostics.Debug.WriteLine(index + " " + s);
+                        }
+                        catch(Exception ex1)
+                        {
+                            System.Diagnostics.Debug.WriteLine(ex1);
+                        }
+                        
+                        index++;
+                    }
+                    sb.Append("}");
+                    System.Diagnostics.Debug.WriteLine(sb.ToString());
+
+                    formatInputStr = convertByteArrayWithNull(data);
+                    //MessageBox.Show(formatInputStr);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error " + ex);
+                }
+                /*}
+                else
+                {
+                    resultChar = crcFromString(txtInput.Text);
+                    byte[] ba = Encoding.Default.GetBytes(resultChar.ToString());
+                    resultStr = byteArrayToHexString(ba);
+                }*/
+                txtResult.Text = resultStr;
+                txtResultHex.Text = resultHex;
+                txtInput.Text = formatInput;
+                txtResultAscii.Text = formatInputStr;
+            }
+        }
+
+        private string convertByteArrayWithNull(byte[] data)
+        {
+            StringBuilder builder = new StringBuilder();
+            int lastOffset = 0;
+            for(int i = 0; i < data.Length; i++)
+            {
+                 if(data[i] == 0)
+                 {
+                    builder.Append(Encoding.UTF8.GetString(data, lastOffset, i - lastOffset));
+                    lastOffset = i + 1;
+                 }
+               // System.Diagnostics.Debug.WriteLine(data.Length + " " + i + " " + lastOffset);
+                //System.Diagnostics.Debug.WriteLine(builder.ToString());
+
+
+            }
+            return builder.ToString();
+        }
+
+        public static byte[] FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            hex = hex.Replace(" ", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+
+        private string byteArrayToHexString(byte[] str)
+        {
+            var hexString = BitConverter.ToString(str);
+            // hexString = hexString.Replace("-", " ");
+            return hexString;
+        }
+
+        private char crcFromString(string source)
+        {
+            int result = 0;
+            for (int i = 0; i < source.Length; i++)
+            {
+                result = result ^ (Byte)(Encoding.Default.GetBytes(source.Substring(i, 1))[0]);
+            }
+            return (Char)result;
+        }
+
+        public static byte crcFromBytes(byte[] bytes)
+        {
+            byte LRC = 0;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                LRC ^= bytes[i];
+            }
+            return LRC;
+        }
+
+        public string ConvertHexToString(string HexValue)
+        {
+            string StrValue = "";
+            while (HexValue.Length > 0)
+            {
+                StrValue += System.Convert.ToChar(System.Convert.ToUInt32(HexValue.Substring(0, 2), 16)).ToString();
+                HexValue = HexValue.Substring(2, HexValue.Length - 2);
+            }
+            return StrValue;
+        }
+
+        public string ConvertStringToHex(string asciiString)
+        {
+            string hex = "";
+            foreach (char c in asciiString)
+            {
+                int tmp = c;
+                hex += String.Format("{0:x2} ", (uint)System.Convert.ToUInt32(tmp.ToString()));
+            }
+            return hex;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                doConvert();
+            }
+        }
+
+        private void txtInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+    }
+}
